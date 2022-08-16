f2b8dd9abebb9520acef6f390c2d570372909d53
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-08-16 20:01:57 -0300

    Desafio

---
+++ calc.js
@@ -0,0 +1,17 @@
+let cart = []
+
+let phone = {
+  name: 'Galaxy Qualquer coisa',
+  price: 3000
+}
+
+let computer = {
+  name: 'Computer',
+  price: 5000
+}
+
+// 1 parte adicionar os 2 produtos em nosso carrinho 
+
+//  Calcular quantos itens temos no carrinho
+
+// Calcular preo final e converter para real
